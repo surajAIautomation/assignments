ab41757893020a78c4cbc3910c1fed5e8d2897df
Suraj Jagtap <suraj.regular@outlook.com>
2025-08-09 00:56:20 +0530

    Assignment 1

--- src/assignments/Assignment1_DataTypes.java
+++ src/assignments/Assignment1_DataTypes.java
@@ -24,10 +24,108 @@ public class Assignment1_DataTypes {
 		String phonenumber = "123-456-7890";
 		System.out.println (phonenumber);
 		
+
+//4. Amount of money in a customer's bank account: 1000.50
+		
+		System.out.print ("4. Amount of money in a customer's bank account : ");
+		float accno = 1000.50f;
+		System.out.println (accno);
+		
+//5. Person's email address: "john.doe@example.com"
+		System.out.print ("5. Person's email address : ");
+		String emailid = "john.doe@example.com";
+		System.out.println(emailid);
+		
+//6. Coordinates of a location (latitude, longitude): 37.7749, -122.4194
+		System.out.println ("6. Coordinates of a location (latitude, longitude) : ");
+		float latitude = 1000.50f;
+		float longitude = -122.4194f;
+		System.out.println (latitude);
+		System.out.println (longitude);
 		
 		
 		
+//7. Person's marital status: true or false
+
+		System.out.print ("7. Person's marital status : ");
+		boolean status 	= true;
+		System.out.println(status);
 		
+//8. Person's occupation: "Software Engineer"
+		
+		System.out.print ("8. Person's occupation : ");
+		String	occupation = "Software Engineer";
+		System.out.println(occupation);
+		
+//9. Person's favourite colour: "Blue"
+		System.out.print ("9. Person's favourite colour : ");
+		String	color = "Blue";
+		System.out.println(color);
+		
+//10. Current year: 2023
+		System.out.print ("10. Current year 2023 : ");
+		short year = 2023;
+		System.out.println (year);
+		
+//11. Number of followers on a social media platform: 1,000,000
+		System.out.print ("11. Number of followers on a social media platform : ");
+		int followers = 1000000;
+		System.out.println(followers);
+		
+//12. Rating of a movie: 7.5
+		System.out.print ("12. Rating of a movie: ");
+		float rating = 7.5f;
+		System.out.println (rating);
+		
+		
+//13 Person's blood type: 'A'
+		System.out.print ("13. Person's blood type: ");
+		char bloodgroup = 'A';
+		System.out.println (bloodgroup);
+		
+//14.Title of a book: "To Kill a Mockingbird"
+		System.out.print ("14.Title of a book : ");
+		String booktitle = "To Kill a Mockingbird";
+		System.out.println (booktitle);
+
+		
+//15. Number of employees in a company: 500
+		System.out.print ("15.Number of employees in a company : ");
+		short employeeNumber = 500;
+		System.out.println(employeeNumber);
+
+		
+//16.Time of an event: 2:30 PM
+		System.out.print ("16.Time of an event: ");
+		String time = "2:30 PM";
+		System.out.println (time);
+		
+//17. Name of a country: "United States";
+		System.out.print ("17. Name of a country: ");
+		String country = "United States";
+		System.out.println (country);
+				
+//18. Person's eye color: "Brown"
+		System.out.print ("18. Person's eye color : ");
+		String eyecolor = "Brown";
+		System.out.println (eyecolor);
+		
+			
+//19.Person's birthplace: "New York City"
+		System.out.print("19.Person's birthplace : ");
+		String birthplace = "New York City";	
+		System.out.println (birthplace);
+		
+//20. Distance between two cities: 200.5
+		System.out.print ("20. Distance between two cities:");
+		float distance = 200.5f;
+		System.out.println (distance);
+		
+
+
+		
+		
+	
 	}
 
 }
