f7cddb994dba1aaed0ff8de74f2a96d5b9f1ff6f
Suraj Jagtap <suraj.regular@outlook.com>
2025-08-08 22:57:43 +0530

    Assignment 1

---
+++ src/assignments/Assignment1_DataTypes.java
@@ -0,0 +1,33 @@
+package assignments;
+
+public class Assignment1_DataTypes {
+
+	public static void main(String[] args) {
+		
+		System.out.print("Hello, ");
+		System.out.println("World!");
+		
+//1. Temperature of a city in degree Celcius: 25.5
+		System.out.print ("1. Temperature of a city in degree Celcius : ");
+		float temperature = 25.5f;
+		System.out.println (temperature);
+		
+		
+//2. Whether a customer has placed an order: true or false
+		System.out.print ("2. Whether a customer has placed an order : ");
+		boolean result = false;
+		System.out.println (result);
+		
+		
+//3. Person's phone number: "123-456-7890"
+		System.out.print ("3. Person's phone number : ");
+		String phonenumber = "123-456-7890";
+		System.out.println (phonenumber);
+		
+		
+		
+		
+		
+	}
+
+}
